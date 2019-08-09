a95f37964d377098d789f0e50b13071920e0763a
Jordy Homing Lam <33080741+jhmlam@users.noreply.github.com>
2019-08-09 11:29:04 +0800

    Update SwapSeabornAxes.py

--- SwapSeabornAxes.py
+++ SwapSeabornAxes.py
@@ -1,1 +1,4 @@
-
+# I will summarise the trick as follow:
+# 1. Retrieve the matplotlib axes object 
+# 2. Copy most axes atttributes (e.g. line data and legend) into a another axes
+# 3. Plot by inverting the x and y
